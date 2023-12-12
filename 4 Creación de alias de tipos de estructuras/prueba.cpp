885952c1b0ece061ab309f7fb8d9f0f3ad757420
nquinbla <nquinbla@myuax.com>
2023-12-12 12:03:33 +0100

    c

--- 4 Creación de alias de tipos de estructuras/prueba.cpp
+++ 4 Creación de alias de tipos de estructuras/prueba.cpp
@@ -1,6 +1,33 @@
 typedef struct Estudiante Estudiante;
 
-Estudiante estudiante3;
-strcpy(estudiante3.nombre, "Pedro");
-estudiante3.edad = 25;
-estudiante3.promedio = 9.7;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Definición de la estructura Estudiante
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+// Crear un alias usando typedef
+typedef Estudiante Alumno;
+
+int main() {
+    // Utilizar el alias Alumno en lugar de Estudiante
+    Alumno estudiante1;
+
+    // Acceder y asignar valores a la estructura usando el alias
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    // Mostrar la información del estudiante
+    cout << "Nombre: " << estudiante1.nombre << endl;
+    cout << "Edad: " << estudiante1.edad << endl;
+    cout << "Promedio: " << estudiante1.promedio << endl;
+
+    return 0;
+}

a125156b1d8eabf97dea902f694ead636a579377
nquinbla <nquinbla@myuax.com>
2023-12-12 12:52:15 +0100

    holi

---
+++ 5 Estructuras y funciones/main.cpp
@@ -0,0 +1,14 @@
+// a. Pasar una estructura por valor como argumento
+//
+//Escribe una función que tome una estructura Estudiante por valor y imprima sus miembros.
+//b. Pasar una estructura por referencia como argumento
+//
+//Modifica la función anterior para que tome una estructura Estudiante por referencia.
+//c. Pasar una estructura por dirección como argumento
+//
+//Modifica la función anterior para que tome una dirección a una estructura Estudiante.
+//d. De la programación funcional a la programación orientada a objetos
+//
+//Este es un concepto más de C++ que de C, pero puedes intentar imaginar cómo podrías organizar el código de la lista de estudiantes en términos de objetos y métodos en lugar de funciones y estructuras.
+
+#include <iostream>

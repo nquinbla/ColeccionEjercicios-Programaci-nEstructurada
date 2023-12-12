becbbd62017254a1161fa2b4e180d83163f11f27
nquinbla <nquinbla@myuax.com>
2023-12-12 13:05:28 +0100

    1

--- 1 Estructuras/main.cpp
+++ 1 Estructuras/main.cpp
@@ -14,15 +14,58 @@
 //
 //Escribe una pequeña aplicación que cree una lista de estudiantes utilizando la estructura Estudiante. Deberías ser capaz de añadir, ver y eliminar estudiantes de la lista.
 
+
 #include <iostream>
-#include <string>
+#include <vector>
+#include <cstdlib>
 
-using namespace std;
-
+// a. Definición de la estructura Estudiante
 struct Estudiante {
-    string nombre;
+    std::string nombre;
     int edad;
     float promedio;
 };
 
+int main() {
+    // b. Instanciación de la estructura Estudiante
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 3.5;
+    std::cout << "Estudiante 1 creado: " << estudiante1.nombre << ", " << estudiante1.edad << ", " << estudiante1.promedio << std::endl;
 
+    // c. Instanciación con malloc
+    Estudiante* estudiante2 = (Estudiante*) malloc(sizeof(Estudiante));
+    estudiante2->nombre = "Pedro";
+    estudiante2->edad = 22;
+    estudiante2->promedio = 3.7;
+    std::cout << "Estudiante 2 creado con malloc: " << estudiante2->nombre << ", " << estudiante2->edad << ", " << estudiante2->promedio << std::endl;
+
+    // d. Puntero a la estructura Estudiante
+    Estudiante* ptrEstudiante = &estudiante1;
+    std::cout << "Puntero a Estudiante 1: " << ptrEstudiante->nombre << ", " << ptrEstudiante->edad << ", " << ptrEstudiante->promedio << std::endl;
+
+    // e. Creación de una lista de estudiantes
+    std::vector<Estudiante> estudiantes;
+
+    // Añadir estudiantes a la lista
+    estudiantes.push_back(estudiante1);
+    estudiantes.push_back(*estudiante2);
+    std::cout << "Estudiantes añadidos a la lista." << std::endl;
+
+    // Ver estudiantes
+    std::cout << "Lista de estudiantes:" << std::endl;
+    for (const auto& estudiante : estudiantes) {
+        std::cout << "Nombre: " << estudiante.nombre << ", Edad: " << estudiante.edad << ", Promedio: " << estudiante.promedio << std::endl;
+    }
+
+    // Eliminar un estudiante
+    estudiantes.pop_back();
+    std::cout << "Último estudiante eliminado de la lista." << std::endl;
+
+    // Liberar memoria
+    free(estudiante2);
+    std::cout << "Memoria liberada para el estudiante 2." << std::endl;
+
+    return 0;
+}

22e62e0a4304a8b1a584c2b8beb765403a0de45a
nquinbla <nquinbla@myuax.com>
2023-12-12 12:56:34 +0100

    a

--- 1 Estructuras/main.cpp
+++ 1 Estructuras/main.cpp
@@ -1,1 +1,28 @@
+//a. Constitución de una estructura
 //
+//Defina una estructura Estudiante que contenga nombre (una cadena de caracteres), edad (un entero) y promedio (un flotante).
+//b. Instanciación de estructuras
+//
+//Crea una instancia de la estructura Estudiante y asigna valores a sus miembros.
+//c. Instanciación con el operador malloc
+//
+//Crea una instancia de la estructura Estudiante utilizando la función malloc.
+//d. Punteros y estructuras
+//
+//Crea un puntero a una estructura Estudiante e inicialízalo.
+//e. Organización de la programación
+//
+//Escribe una pequeña aplicación que cree una lista de estudiantes utilizando la estructura Estudiante. Deberías ser capaz de añadir, ver y eliminar estudiantes de la lista.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+

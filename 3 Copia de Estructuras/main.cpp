36bf6031870a0032d9168b06d85b850d5cfdbbd4
nquinbla <nquinbla@myuax.com>
2023-12-12 11:55:30 +0100

    b

---
+++ 3 Copia de Estructuras/main.cpp
@@ -0,0 +1,35 @@
+// Escribe una función que tome una estructura Estudiante y devuelva una copia de la misma.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Estudiante {
+    string nombre;
+    int edad;
+    int curso;
+};
+
+Estudiante copiaEstudiante(Estudiante estudiante) {
+    return estudiante;
+}
+
+int main() {
+    Estudiante estudiante1;
+
+    cout << "Introduce el nombre del estudiante: ";
+    cin >> estudiante1.nombre;
+    cout << "Introduce la edad del estudiante: ";
+    cin >> estudiante1.edad;
+    cout << "Introduce el curso del estudiante: ";
+    cin >> estudiante1.curso;
+
+    Estudiante estudiante2 = copiaEstudiante(estudiante1);
+
+    cout << "Nombre: " << estudiante2.nombre << endl;
+    cout << "Edad: " << estudiante2.edad << endl;
+    cout << "Curso: " << estudiante2.curso << endl;
+
+    return 0;
+}
